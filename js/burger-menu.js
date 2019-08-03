7d3792e8e9e2b733359338ea59bd66e8c964fb5c
Istvudlee <istvudlee@mail.ru>
2019-08-03 18:08:19 +0300

    init

---
+++ js/burger-menu.js
@@ -0,0 +1,28 @@
+// Бургер
+var mobbar = document.querySelector('.mob-nav-bar');
+var burger = document.querySelector('.button_burger');
+burger.onclick = function() {
+	mobbar.classList.toggle('activmob-nav-bar');
+	burger.classList.toggle('closemobburger');
+}
+// Табы (Дать на проверку)  = переписать под "делегировагие событий"
+var toogletab = document.getElementById('toogletab');
+var toogle = toogletab.querySelectorAll('#tog2, #tog1, #tog3');
+var div = document.querySelectorAll('.tabcontent')
+toogletab.onclick = function(e) {
+	var target = e.target; 
+	if(target.classList === 'active') return;
+	for (i=0; i < toogletab.children.length; i++) {
+    if (toogle[i].classList.contains("active")) {
+			toogle[i].classList.remove("active");
+		}
+	}
+	target.classList.add('active');
+  for(i = 0; i < div.length; i++ ) {
+   div[i].classList.remove("visible")
+	 if(target.dataset.number === div[i].dataset.number) {
+		div[i].classList.add("visible");
+	 }
+	}
+}
+// 
